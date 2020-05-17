2112da1c2890cec3e79e6c7672a145a9c37dde21
ori229 <millero@gmail.com>
2020-05-17 10:48:46 +0300

    Upgrade to exl-cloudapp-base 0.1.20

--- cloudapp/src/app/main/main.component.ts
+++ cloudapp/src/app/main/main.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatCheckboxChange } from '@angular/material';
+import { MatCheckboxChange } from '@angular/material/checkbox'; 
 import { Subscription } from 'rxjs';
 import { HttpClient } from '@angular/common/http'; 
 import {
