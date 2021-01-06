313b538d42fd45a41fbeff6963121b758937f0db
ori229 <millero@gmail.com>
2021-01-06 11:19:06 +0200

    Add some more lines to the Help page

--- cloudapp/src/app/help/help.component.ts
+++ cloudapp/src/app/help/help.component.ts
@@ -10,6 +10,11 @@ import { Component, OnInit } from '@angular/core';
     <h1>Help</h1>
   </div>
   <div>
+    <p>The App uses XSL to convert MARCXML records to HTML.</p>
+    <p>It has 3 built-in XSLs and supports adding one custom XSL.</p>
+    <p>Under Settings you can select one of the pre-built XSLs.</p>
+    <p>Under Configuration (available for Admin users) - you can create a custom XSL. Use the option to “Pick an XSL file to use as a starting point”, provide a name and save.</p>
+    <p>For details see <a href="https://developers.exlibrisgroup.com/blog/how-to-make-a-custom-format-in-the-print-bib-records-cloud-app/" target="_blank">this blog post</a>.</p>
     <p>For more help with this app, or to report a problem, please open an issue by clicking on the link below.</p>
     <p><a translate href="https://github.com/ori229/print-bib-record/issues" target="_blank">Open an issue</a></p>
   </div>
